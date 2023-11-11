50615710e54837b2c3e5a45a7e0e01cca336a7f6
AfiqRazak <afiqrazak380@gmail.com>
2023-11-11 12:39:18 +0000

    Inline and modular component.

---
+++ React_FullCourse/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Greeting from "./Greeting"
+
+function App() {
+
+  const Name = () => <h1>Herry</h1>
+
+ return(
+    <>
+      <Greeting name="Herry" ></Greeting>
+      <Name></Name>
+    </>
+ )
+}
+
+export default App
