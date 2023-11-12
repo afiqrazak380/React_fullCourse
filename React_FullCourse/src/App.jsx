f615a24551a430d92556e22682efff95c0605cdf
AfiqRazak <afiqrazak380@gmail.com>
2023-11-12 15:58:05 +0000

    Component in Module and InLine

--- React_FullCourse/src/App.jsx
+++ React_FullCourse/src/App.jsx
@@ -1,16 +1,33 @@
-
-import Greeting from "./Greeting"
+import Book from "./Book";
 
 function App() {
+  const Name = () => <h1>Herry</h1>;
 
-  const Name = () => <h1>Herry</h1>
-
- return(
+  return (
     <>
-      <Greeting name="Herry" ></Greeting>
-      <Name></Name>
+      <Book></Book>
+      <Book1></Book1>
     </>
- )
+  );
 }
 
-export default App
+export default App;
+
+const Book1 = () => {
+  return (
+    <article>
+      <Title></Title>
+      <Image></Image>
+      <Author></Author>
+    </article>
+  );
+};
+
+const Title = () => <h1>Titanic Coloring Book</h1>;
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/71q3eq1MSiL._SY342_.jpg"
+    alt=";"
+  ></img>
+);
+const Author = () => <h4>Titanic Author</h4>;
---
+++ React_FullCourse/src/Book.jsx
@@ -0,0 +1,13 @@
+const Book = (props) => {
+  return (
+    <section>
+      <h1>Titanic Coloring Book1</h1>
+      <img src="https://m.media-amazon.com/images/I/71q3eq1MSiL._SY342_.jpg"></img>
+      <h4 className="Author">Titanic Author1</h4>
+
+      <></>
+    </section>
+  );
+};
+
+export default Book;
